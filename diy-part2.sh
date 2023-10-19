4a93e398e5a7b362919ab169ee4fbcd7a9b1527c
sonytxl <58774013+sonytxl@users.noreply.github.com>
2023-10-19 10:45:19 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,10 +12,11 @@
 
 # 自定义默认网关，后方的192.168.61.1即是可自定义的部分
 sed -i 's/192.168.1.1/192.168.61.1/g' package/base-files/files/bin/config_generate
+
 #sed -i "s/hostname='ImmortalWrt'/hostname='360T7'/g" package/base-files/files/bin/config_generate
 
 # 固件版本名称自定义
-sed -i "s/DISTRIB_DESCRIPTION=.*/DISTRIB_DESCRIPTION='OpenWrt By gino $(date +"%Y%m%d")'/g" package/base-files/files/etc/openwrt_release
+#sed -i "s/DISTRIB_DESCRIPTION=.*/DISTRIB_DESCRIPTION='OpenWrt By gino $(date +"%Y%m%d")'/g" package/base-files/files/etc/openwrt_release
 
 # 取消原主题luci-theme-bootstrap 为默认主题
 sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
