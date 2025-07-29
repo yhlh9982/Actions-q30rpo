b967497c441964d59294861ffc087d74a9fbe6b6
Otto kai <121492489+yhlh9982@users.noreply.github.com>
2025-07-29 15:18:46 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,45 +1,62 @@
 #!/bin/bash
+
+#========================================================================================================================
 #
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part1.sh
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
+# F-WRT DIY-Part1 Script
 #
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
+# Copyright (c) 2019-2024 F-WRT
 #
 # This is free software, licensed under the MIT License.
 # See /LICENSE for more information.
 #
+# https://github.com/f-wrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
+#========================================================================================================================
 
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# ----------------------------------------------------------------------------------------------------------------------
+# 添加自定义软件包
+#
+# 该脚本在 `feeds update` 之前运行。
+#
+# 通常，您可以在这里：
+# 1. 添加第三方软件包源到 `feeds.conf.default` 文件。
+# 2. 使用 `git clone` 将独立的软件包克隆到 `package/` 目录下。
+#
+# 请注意：
+# - 克隆到 `package/` 目录下的软件包拥有最高优先级，它会“覆盖”掉来自 feeds 的同名软件包。
+# - 所有的 git clone 命令都应指定目标路径为 `package/` 子目录。
+#
+# ----------------------------------------------------------------------------------------------------------------------
 
-# Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld.git' >> feeds.conf.default
-#echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' >>feeds.conf.default
-echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main' >>feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' >>feeds.conf.default
-echo 'src-git openclash https://github.com/vernesong/OpenClash.git;dev' >> feeds.conf.default
-echo 'src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main' >> feeds.conf.default
-#echo 'src-git luci-app-smartdns https://github.com/pymumu/luci-app-smartdns.git;master' >>feeds.conf.default
-#echo 'src-git openwrt-smartdns https://github.com/pymumu/openwrt-smartdns.git;master' >>feeds.conf.default
 
-#git clone --depth=1 -b main https://github.com/fw876/helloworld package/luci-app-ssr-plus
-#git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/opasswall-packages
-#git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
-#git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2 package/luci-app-passwall2
-#git clone --depth=1 -b dev https://github.com/vernesong/OpenClash package/luci-app-openclash
-#git clone --depth=1 https://github.com/nikkinikki-org/OpenWrt-nikki package/luci-app-nikki
+# 示例：添加一个第三方 feed 源
+# sed -i '$a src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages;main' feeds.conf.default
+# sed -i '$a src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2;main' feeds.conf.default
 
-#git clone --depth=1 https://github.com/pymumu/luci-app-smartdns package/luci-app-smartdns
-#git clone --depth=1 https://github.com/pymumu/openwrt-smartdns package/smartdns
 
-git clone --depth=1 -b js https://github.com/sirpdboy/luci-app-poweroffdevice package/luci-app-poweroffdevice     #关机
-git clone --depth=1 -b main https://github.com/lwb1978/openwrt-gecoosac package/openwrt-gecoosac   #集客
-git clone --depth=1 -b main https://github.com/sirpdboy/luci-app-netwizard package/luci-app-netwizard   #网络设置向导
-git clone --depth=1 -b js https://github.com/sirpdboy/luci-theme-kucat package/luci-theme-kucat
-git clone --depth=1 -b main https://github.com/sirpdboy/luci-app-advancedplus package/luci-app-advancedplus
-git clone --depth=1 -b main https://github.com/oppen321/luci-app-wolplus package/luci-app-wolplus
-git clone --depth=1 https://github.com/sirpdboy/luci-app-partexp.git package/luci-app-partexp
+# ----------------------------------------------------------------------------------------------------------------------
+# 使用 git clone 添加独立的软件包
+# 格式: git clone --depth=1 [仓库地址] package/[目录名]
+# ----------------------------------------------------------------------------------------------------------------------
+
+echo " "
+echo "========================================================================================================================"
+echo " Cloning custom packages..."
+echo "========================================================================================================================"
+echo " "
+
+# 主题
+git clone --depth=1 https://github.com/sirpdboy/luci-theme-kucat.git package/luci-theme-kucat
+git clone --depth=1 https://github.com/sirpdboy/luci-app-advancedplus.git package/luci-app-advancedplus
+
+# 科学上网插件
+git clone --depth=1 https://github.com/nikkinikki-org/OpenWrt-nikki.git package/nikki
+git clone --depth=1 -b dev https://github.com/vernesong/OpenClash.git package/openclash
+#git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages.git package/passwall-packages
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall.git package/passwall
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2.git package/passwall2
 
 WORKINGDIR="`pwd`/feeds/packages/net/smartdns"
 mkdir $WORKINGDIR -p
@@ -60,7 +77,17 @@ mv $WORKINGDIR/luci-app-smartdns-${LUCIBRANCH}/* $WORKINGDIR/
 rmdir $WORKINGDIR/luci-app-smartdns-${LUCIBRANCH}
 rm $WORKINGDIR/${LUCIBRANCH}.zip
 
-# 更新 golang 1.25 版本
-#rm -rf feeds/packages/lang/golang
-#git clone https://github.com/sbwml/packages_lang_golang -b 25.x feeds/packages/lang/golang
+# 常用工具与应用
+git clone --depth=1 https://github.com/sirpdboy/luci-app-poweroffdevice.git package/luci-app-poweroffdevice # 关机功能
+git clone --depth=1 -b main https://github.com/oppen321/luci-app-wolplus package/luci-app-wolplus
+git clone --depth=1 https://github.com/asvow/luci-app-tailscale.git package/luci-app-tailscale # Tailscale
+git clone --depth=1 https://github.com/lwb1978/openwrt-gecoosac.git package/gecoosac # 集客 AC 控制器
+git clone --depth=1 https://github.com/sirpdboy/luci-app-partexp.git package/luci-app-partexp # 网络引导/分区扩展
+git clone --depth=1 https://github.com/EasyTier/luci-app-easytier.git package/easytier # EasyTier
 
+
+echo " "
+echo "========================================================================================================================"
+echo " All custom packages have been cloned successfully."
+echo "========================================================================================================================"
+echo " "
