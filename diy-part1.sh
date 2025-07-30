283a8112c59e867a62132cb341a67df88a452c99
Otto kai <121492489+yhlh9982@users.noreply.github.com>
2025-07-30 11:06:54 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -58,10 +58,10 @@ git clone --depth=1 https://github.com/asvow/luci-app-tailscale.git package/luci
 git clone --depth=1 https://github.com/lwb1978/openwrt-gecoosac.git package/gecoosac # 集客 AC 控制器
 git clone --depth=1 https://github.com/sirpdboy/luci-app-partexp.git package/luci-app-partexp # #一键自动格式化分区、扩容、自动挂载插件
 git clone --depth=1 https://github.com/EasyTier/luci-app-easytier.git package/luci-app-easytier # EasyTier
-git clone --depth=1 https://github.com/destan19/OpenAppFilter package/luci-app-oaf  # 应用过滤(OAF)
-git clone --depth=1 https://github.com/sirpdboy/luci-app-taskplan package/luci-app-taskplan   #任务设置2.0版
-git clone --depth=1 https://github.com/sirpdboy/luci-app-watchdog package/luci-app-watchdog  #看门狗
-git clone --depth=1 https://github.com/KyleRicardo/MentoHUST-OpenWrt-ipk package/luci-app-mentohust  #锐捷验证 luci-app-mentohust
+git clone --depth=1 https://github.com/destan19/OpenAppFilter.git package/luci-app-oaf  # 应用过滤(OAF)
+git clone --depth=1 https://github.com/sirpdboy/luci-app-taskplan.git package/luci-app-taskplan   #任务设置2.0版
+git clone --depth=1 https://github.com/sirpdboy/luci-app-watchdog.git package/luci-app-watchdog  #看门狗
+git clone --depth=1 https://github.com/KyleRicardo/MentoHUST-OpenWrt-ipk.git package/luci-app-mentohust  #锐捷验证 luci-app-mentohust
 
 # 科学上网插件
 # nikki
