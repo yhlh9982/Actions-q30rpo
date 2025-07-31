54b92a9ff3f1d27ced301b9b79bac065ce5e9f9d
Otto kai <121492489+yhlh9982@users.noreply.github.com>
2025-07-31 17:53:11 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,15 +13,6 @@
 # ---------------------------------------------------------------------------------
 #  Rust 编译if-unchanged
 # ---------------------------------------------------------------------------------
-echo "==> Applying patch for Rust build issue..."
-RUST_MAKEFILE=$(find feeds/packages/lang/rust -type f -name "Makefile")
-if [ -f "$RUST_MAKEFILE" ]; then
-    # 查找包含 'download-ci-llvm' 的行，并将其值从 true 替换为 if-unchanged
-    sed -i 's/download-ci-llvm\s*:=\s*true/download-ci-llvm := if-unchanged/g' "$RUST_MAKEFILE"
-    echo "Rust Makefile patched successfully at: $RUST_MAKEFILE"
-else
-    echo "Warning: Rust Makefile not found, skipping patch."
-fi
 
 # 自定义默认网关，后方的192.168.30.1即是可自定义的部分
 sed -i 's/192.168.6.1/192.168.30.1/g' package/base-files/files/bin/config_generate
